a69deaba888cd2d92c22c57a5d76d889441276d3
Tiago <tiago.salles@houseasy.net>
2022-07-06 21:19:10 -0300

    Refactor: Refatored HomePage code

--- lib/app/presenter/pages/home/home_page.dart
+++ lib/app/presenter/pages/home/home_page.dart
@@ -1,119 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:read_text/app/core/app_module/app_modules.dart';
 import 'package:read_text/app/core/model/text_model.dart';
+import 'package:read_text/app/core/utils/languages_utils.dart';
 import 'package:read_text/app/domain/domains/home_domain.dart';
-
-final langs = {
-  'auto': 'Automatic',
-  'af': 'Afrikaans',
-  'sq': 'Albanian',
-  'am': 'Amharic',
-  'ar': 'Arabic',
-  'hy': 'Armenian',
-  'az': 'Azerbaijani',
-  'eu': 'Basque',
-  'be': 'Belarusian',
-  'bn': 'Bengali',
-  'bs': 'Bosnian',
-  'bg': 'Bulgarian',
-  'ca': 'Catalan',
-  'ceb': 'Cebuano',
-  'ny': 'Chichewa',
-  'zh-cn': 'Chinese Simplified',
-  'zh-tw': 'Chinese Traditional',
-  'co': 'Corsican',
-  'hr': 'Croatian',
-  'cs': 'Czech',
-  'da': 'Danish',
-  'nl': 'Dutch',
-  'en': 'English',
-  'eo': 'Esperanto',
-  'et': 'Estonian',
-  'tl': 'Filipino',
-  'fi': 'Finnish',
-  'fr': 'French',
-  'fy': 'Frisian',
-  'gl': 'Galician',
-  'ka': 'Georgian',
-  'de': 'German',
-  'el': 'Greek',
-  'gu': 'Gujarati',
-  'ht': 'Haitian Creole',
-  'ha': 'Hausa',
-  'haw': 'Hawaiian',
-  'iw': 'Hebrew',
-  'hi': 'Hindi',
-  'hmn': 'Hmong',
-  'hu': 'Hungarian',
-  'is': 'Icelandic',
-  'ig': 'Igbo',
-  'id': 'Indonesian',
-  'ga': 'Irish',
-  'it': 'Italian',
-  'ja': 'Japanese',
-  'jw': 'Javanese',
-  'kn': 'Kannada',
-  'kk': 'Kazakh',
-  'km': 'Khmer',
-  'ko': 'Korean',
-  'ku': 'Kurdish (Kurmanji)',
-  'ky': 'Kyrgyz',
-  'lo': 'Lao',
-  'la': 'Latin',
-  'lv': 'Latvian',
-  'lt': 'Lithuanian',
-  'lb': 'Luxembourgish',
-  'mk': 'Macedonian',
-  'mg': 'Malagasy',
-  'ms': 'Malay',
-  'ml': 'Malayalam',
-  'mt': 'Maltese',
-  'mi': 'Maori',
-  'mr': 'Marathi',
-  'mn': 'Mongolian',
-  'my': 'Myanmar (Burmese)',
-  'ne': 'Nepali',
-  'no': 'Norwegian',
-  'ps': 'Pashto',
-  'fa': 'Persian',
-  'pl': 'Polish',
-  'pt': 'Portuguese',
-  'pa': 'Punjabi',
-  'ro': 'Romanian',
-  'ru': 'Russian',
-  'sm': 'Samoan',
-  'gd': 'Scots Gaelic',
-  'sr': 'Serbian',
-  'st': 'Sesotho',
-  'sn': 'Shona',
-  'sd': 'Sindhi',
-  'si': 'Sinhala',
-  'sk': 'Slovak',
-  'sl': 'Slovenian',
-  'so': 'Somali',
-  'es': 'Spanish',
-  'su': 'Sundanese',
-  'sw': 'Swahili',
-  'sv': 'Swedish',
-  'tg': 'Tajik',
-  'ta': 'Tamil',
-  'te': 'Telugu',
-  'th': 'Thai',
-  'tr': 'Turkish',
-  'uk': 'Ukrainian',
-  'ur': 'Urdu',
-  'uz': 'Uzbek',
-  'ug': 'Uyghur',
-  'vi': 'Vietnamese',
-  'cy': 'Welsh',
-  'xh': 'Xhosa',
-  'yi': 'Yiddish',
-  'yo': 'Yoruba',
-  'zu': 'Zulu'
-};
+import 'package:read_text/app/presenter/pages/home/widgets/word_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -122,228 +12,270 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-StreamController<bool> readOnlyStreamController =
-    StreamController<bool>.broadcast();
-
 class _HomePageState extends State<HomePage> {
   late final HomeDomain homeDomain;
   late TextEditingController textController;
-  late TextEditingController fullTextController;
 
   @override
   void initState() {
     homeDomain = AppModules.homeDomain;
     textController = TextEditingController();
-    fullTextController = TextEditingController();
+
     super.initState();
   }
 
-  bool readOnly = false;
-  bool selectedWord = false;
-  List<String> moreThanTwoWordsList = [];
-  String? language;
-  String? item;
-  final List<DropdownMenuItem> dropdownItems = [];
+  List<String> moreThanOneWordList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            const SizedBox(height: 40),
-            Row(
+      body: BodyHomePage(
+        homeDomain: homeDomain,
+        moreThanOneWordList: moreThanOneWordList,
+      ),
+      bottomSheet: HomeBottomSheet(
+        homeDomain: homeDomain,
+        moreThanOneWordList: moreThanOneWordList,
+      ),
+    );
+  }
+}
+
+class HomeBottomSheet extends StatelessWidget {
+  const HomeBottomSheet({
+    Key? key,
+    required this.homeDomain,
+    required this.moreThanOneWordList,
+  }) : super(key: key);
+
+  final HomeDomain homeDomain;
+  final List<String> moreThanOneWordList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      height: 120,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Colors.blue[200],
+        borderRadius: const BorderRadius.only(
+          topRight: Radius.circular(60),
+        ),
+      ),
+      child: ValueListenableBuilder<TextModel?>(
+        valueListenable: homeDomain.textListenable,
+        builder: (context, textModel, widget) {
+          if (textModel?.translatedText == null) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "Escolha uma palavra para traduzir",
+                ),
+              ],
+            );
+          } else {
+            return Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                DropdownButton<String>(
-                  value: item ?? "English",
-                  items: langs.values
-                      .map(
-                        (value) => DropdownMenuItem(
-                          value: value,
-                          onTap: () {},
-                          enabled: true,
-                          child: SizedBox(child: Text(value)),
-                        ),
-                      )
-                      .toList(),
-                  onChanged: (itemValue) {
-                    setState(() {
-                      item = itemValue;
-                      langs.forEach((key, value) {
-                        if (value == itemValue) {
-                          language = key;
-                        }
-                      });
-                    });
-                  },
-                ),
-                const SizedBox(width: 50),
                 Container(
-                  margin: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.blue[200],
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        readOnly = !readOnly;
-                      });
-
-                      readOnlyStreamController.sink.add(readOnly);
-                    },
+                  padding: const EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: SingleChildScrollView(
                     child: Text(
-                      readOnly == false ? "Modo leitura" : "Adicionar texto",
+                      "${textModel?.translatedText}",
                     ),
+                  ),
+                ),
+                const SizedBox(width: 30),
+                IconButton(
+                  onPressed: () {
+                    moreThanOneWordList.clear();
+                    TextModel textModel = TextModel();
+                    homeDomain.textListenable.value = textModel;
+                  },
+                  iconSize: 50,
+                  icon: const Icon(Icons.highlight_remove_sharp),
+                )
+              ],
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class BodyHomePage extends StatefulWidget {
+  const BodyHomePage({
+    Key? key,
+    required this.homeDomain,
+    required this.moreThanOneWordList,
+  }) : super(key: key);
+  final HomeDomain homeDomain;
+  final List<String> moreThanOneWordList;
+
+  @override
+  State<BodyHomePage> createState() => _BodyHomePageState();
+}
+
+class _BodyHomePageState extends State<BodyHomePage> {
+  late TextEditingController fullTextController;
+
+  @override
+  void initState() {
+    fullTextController = TextEditingController();
+    super.initState();
+  }
+
+  bool selectedWord = false;
+  String item = LanguageUtils.languages["en"]!;
+  String language = "en";
+  List<DropdownMenuItem<String>> dropDownItems = [];
+
+  @override
+  Widget build(BuildContext context) {
+    for (String language in LanguageUtils.languages.values) {
+      dropDownItems.add(
+        DropdownMenuItem(
+          value: language,
+          onTap: () {},
+          enabled: true,
+          child: SizedBox(
+            child: Text(language),
+          ),
+        ),
+      );
+    }
+
+    return SingleChildScrollView(
+      child: ValueListenableBuilder<bool>(
+          valueListenable: widget.homeDomain.readOnlyListenable,
+          builder: (context, readOnly, _) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                const SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    DropdownButton<String>(
+                      value: item,
+                      items: dropDownItems,
+                      onChanged: (itemValue) {
+                        setState(() {
+                          item = itemValue!;
+                          LanguageUtils.languages.forEach((key, value) {
+                            if (value == itemValue) {
+                              language = key;
+                            }
+                          });
+                        });
+                      },
+                    ),
+                    const SizedBox(width: 50),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.blue[200],
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: TextButton(
+                        onPressed: () {
+                          bool readOnly =
+                              !widget.homeDomain.readOnlyListenable.value;
+                          widget.homeDomain.readOnlyListenable.value = readOnly;
+                        },
+                        child: Text(
+                          readOnly == false
+                              ? "Modo leitura"
+                              : "Adicionar texto",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  margin: const EdgeInsets.all(10),
+                  height: 600,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 255, 246, 197),
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 5,
+                      )
+                    ],
+                  ),
+                  child: SingleChildScrollView(
+                    child: readOnly
+                        ? ReadModeComponent(
+                            wordsToSeparete: fullTextController.text,
+                            homeDomain: widget.homeDomain,
+                            language: language,
+                            moreThanOneWordList: widget.moreThanOneWordList,
+                          )
+                        : TextFormField(
+                            maxLines: null,
+                            controller: fullTextController,
+                            decoration: const InputDecoration(
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ),
+                          ),
                   ),
                 ),
               ],
-            ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              margin: const EdgeInsets.all(10),
-              height: 600,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 255, 246, 197),
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 5,
-                  )
-                ],
-              ),
-              child: SingleChildScrollView(
-                child: StreamBuilder<bool>(
-                  stream: readOnlyStreamController.stream,
-                  builder: (context, snapshot) {
-                    if (snapshot.data == true) {
-                      List<String> words = fullTextController.text.split(" ");
-                      List<Widget> children = [];
-                      Widget? wordButton;
+            );
+          }),
+    );
+  }
+}
 
-                      for (String word in words) {
-                        wordButton = GestureDetector(
-                          onTap: () {
-                            HapticFeedback.mediumImpact();
-                            homeDomain.translate(word, language);
-                          },
-                          onLongPress: () {
-                            if (moreThanTwoWordsList.contains("$word ")) {
-                              if (moreThanTwoWordsList.length == 1) {
-                                HapticFeedback.vibrate();
-                                moreThanTwoWordsList.remove("$word ");
-                                TextModel textModel = TextModel();
-                                homeDomain.textStreamController.sink
-                                    .add(textModel);
-                              } else {
-                                HapticFeedback.vibrate();
-                                moreThanTwoWordsList.remove("$word ");
-                                String phrase = moreThanTwoWordsList.join();
-                                homeDomain.translate(phrase, language);
-                              }
-                            } else {
-                              HapticFeedback.vibrate();
-                              moreThanTwoWordsList.add("$word ");
-                              String phrase = moreThanTwoWordsList.join();
-                              homeDomain.translate(phrase, language);
-                            }
-                          },
-                          child: Text(
-                            "$word  ",
-                            style: const TextStyle(
-                              fontSize: 18,
-                              color: Colors.black87,
-                            ),
-                          ),
-                        );
-                        children.add(wordButton);
-                      }
+class ReadModeComponent extends StatelessWidget {
+  const ReadModeComponent({
+    Key? key,
+    required this.wordsToSeparete,
+    required this.homeDomain,
+    required this.language,
+    required this.moreThanOneWordList,
+  }) : super(key: key);
 
-                      return Wrap(
-                        children: children,
-                      );
-                    } else {
-                      return TextFormField(
-                        readOnly: readOnly,
-                        maxLines: null,
-                        controller: fullTextController,
-                        decoration: InputDecoration(
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                              color: readOnly == true
-                                  ? Colors.transparent
-                                  : Colors.grey,
-                            ),
-                          ),
-                          focusedBorder: const UnderlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ),
-                      );
-                    }
-                  },
-                ),
-              ),
-            ),
-          ],
+  final String wordsToSeparete;
+  final HomeDomain homeDomain;
+  final String language;
+  final List<String> moreThanOneWordList;
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> words = wordsToSeparete.split(" ");
+    List<Widget> children = [];
+
+    for (String word in words) {
+      children.add(
+        WordButton(
+          homeDomain: homeDomain,
+          word: word,
+          language: language,
+          moreThanOneWordList: moreThanOneWordList,
         ),
-      ),
-      bottomSheet: Container(
-        padding: const EdgeInsets.all(10),
-        height: 120,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          color: Colors.blue[200],
-          borderRadius: const BorderRadius.only(
-            topRight: Radius.circular(60),
-          ),
-        ),
-        child: StreamBuilder<TextModel?>(
-          stream: homeDomain.textStreamController.stream,
-          builder: (context, snapshot) {
-            if (snapshot.data?.translatedText == null) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    "Escolha uma palavra para traduzir",
-                  ),
-                ],
-              );
-            } else {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    width: MediaQuery.of(context).size.width * 0.7,
-                    child: SingleChildScrollView(
-                      child: Text(
-                        "${snapshot.data?.translatedText}",
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 30),
-                  IconButton(
-                    onPressed: () {
-                      moreThanTwoWordsList.clear();
-                      TextModel textModel = TextModel();
-                      homeDomain.textStreamController.sink.add(textModel);
-                    },
-                    iconSize: 50,
-                    icon: const Icon(Icons.highlight_remove_sharp),
-                  )
-                ],
-              );
-            }
-          },
-        ),
-      ),
+      );
+    }
+
+    return Wrap(
+      children: children,
     );
   }
 }
